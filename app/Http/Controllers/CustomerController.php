33f6192a198aeec6c4b9233728c04178f7579afb
alishapathak <alishap314@gmail.com>
2018-04-22 08:33:48 +0545

    ...

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 4/7/2018
+ * Time: 9:09 AM
+ */
+
+namespace App\Http\Controllers;
+
+
+use App\Customer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class Customercontroller extends Controller
+{
+
+    public function index()
+    {
+        $customers= Customer::all();
+        Log::debug("customers retrieved",['size'=>count($customers)]);
+        Log::info("successfully retrieved customers");
+        return view('customer.index',compact('customers'));
+    }
+    public function show($id)
+    {
+        return view('customer.show', ['customer'=>Customer::findOrFail($id)]);
+    }
+
+    public function create(){
+        return view('customer.create');
+    }
+
+    public function store(Request $request)
+    {
+        $customer= Customer::create($request->all());
+        return redirect('customer/show/'. $customer->customer_id);
+    }
+}
---
+++ resources/views/customer/show.blade.php
@@ -0,0 +1,4 @@
+
+<a href="{{route('customer.index')}}">View All Data</a></br>
+{{$customer->first_name}}
+{{$customer->last_name}}
